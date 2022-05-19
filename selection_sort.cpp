0fe9b56c7f4c5207b40bd05274e82b7dedb8a01d
LidiaS <39269109+LSSYA@users.noreply.github.com>
2022-05-19 20:17:43 +0300

    Create selection_sort.cpp

---
+++ selection_sort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Solution
+{   
+    void selectionSort(int arr[], int n)
+    {
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+};
