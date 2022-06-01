d8b9073c108cea4bcde2aef8002df615c0e30378
LidiaS <39269109+LSSYA@users.noreply.github.com>
2022-06-01 23:32:21 +0300

    Time: 4 ms (30.71%), Space: 5.9 MB (44.85%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    double myPow(double x, int n) 
-   {
+    double myPow(double x, long long n) 
+    {
+        if(n<0)
+        {
+            n *= -1;
+            x = 1/x;
+        }
         if(n==0)
             return 1;
-        
-    return (n<0) ? helper(1/x, (-1 * (long long)n)) : helper(x,n);
-        
-    }
-    double helper(double x, long long n)
-    {
-        if(x==0)
-            return 0;
         if(n==1)
             return x;
-        int mid = n/2;
-        double temp  = helper(x *x, n/2);
-        return temp * ((n%2) ? x : 1);
+        
+        return myPow(x*x, n/2) * ((n%2) ? x : 1);
     }
 };
