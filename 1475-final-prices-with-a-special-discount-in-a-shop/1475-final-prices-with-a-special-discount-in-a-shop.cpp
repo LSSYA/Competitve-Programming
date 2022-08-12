f900b909dad0faad4163884ada3366b1603fecb7
LidiaS <39269109+LSSYA@users.noreply.github.com>
2022-08-12 19:53:29 +0300

    Time: 9 ms (44.06%), Space: 10 MB (56.74%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int>sk;
-        for(int i=0;i<prices.size();i++){
-        int discount=prices[i];
-            for(int j=i+1;j<prices.size();j++){
-                if(prices[i]>=prices[j]){
-                    discount=prices[i]-prices[j];
+        
+        vector <int> discount;
+        
+        for(int i=0; i<prices.size(); i++)
+        {
+            int disp = prices[i];
+            
+            for(int j=i+1; j<prices.size(); j++)
+            {
+                  if(prices[j]<=prices[i])
+                {
+                    disp = prices[i] - prices[j];
                     break;
                 }
                 
             }
-            sk.push_back(discount);
+            
+            discount.push_back(disp);
         }
-        return sk;
+        
+        return discount;
     }
 };
